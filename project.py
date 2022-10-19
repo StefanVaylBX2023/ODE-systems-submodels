eac2ac1ce0a229f9be3857a0514e25faa5586c55
StefanVaylBX2023 <91256224+StefanVaylBX2023@users.noreply.github.com>
2022-10-20 00:33:10 +0200

    Update project.py

--- project.py
+++ project.py
@@ -5,7 +5,8 @@ f = open('input.txt', 'r')
 x = []
 y = []
 graph = {}
-
+ind = {}
+count = 0
 for line in f:
     line = line.split('=')
     line[0] = list(exp(line[0]).free_symbols)[0]
@@ -15,9 +16,11 @@ for line in f:
         #print(line[0])
         line[0] = Symbol(line[0])
     graph[line[0]] = list(exp(line[1]).free_symbols)
+    ind[line[0]] = count
+    count += 1
 
 f.close()
-
+print(ind)
 
 print(graph)
 
@@ -64,11 +67,36 @@ for y in Y:
             
         
 
-print(1)
-#print(models)
-#for i in range(len(models)):
 print(models)
 
+res = []
+for y in Y:
+    res.append(models[y])
+
+print(res)
+ln = []
+for i in res:
+    il = []
+    for j in i:
+        il.append(ind[j])
+    ln.append(il)
+
+print(ln)
+
+# take line in file based on index
+f = open('input.txt', 'r')
+lines = f.readlines()
+f.close()
+
+# write to file
+f = open('output.txt', 'w')
+for i in ln:
+    f.write('Submodel ' + str(ln.index(i) + 1) + ':')
+    f.write('\n')
+    for j in i:
+        f.write(lines[j])
+    #new line in file
+    f.write('\n')
 
 
 
