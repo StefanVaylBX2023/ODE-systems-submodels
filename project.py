53fe22049547600d4b0270d753ed4bf67441a4f7
StefanVaylBX2023 <91256224+StefanVaylBX2023@users.noreply.github.com>
2022-10-21 16:28:12 +0200

    Update project.py

--- project.py
+++ project.py
@@ -1,45 +1,44 @@
+
 from sympy import *
+import sys
+import time
 
-f = open('input.txt', 'r')
-x = []
-y = []
-graph = {}
-ind = {}
-count = 0
-for line in f:
-    line = line.split('=')
-    line[0] = list(exp(line[0]).free_symbols)[0]
-    #print(line[0])
-    if str(line[0]).startswith('d'):
-        line[0] = str(line[0]).split('d')[1]
+def read_input(filename):
+    f = open(f'{filename}', 'r')
+    graph = {}
+    ind = {}
+    count = 0
+    for line in f:
+        line = line.split('=')
+        line[0] = list(exp(line[0]).free_symbols)[0]
         #print(line[0])
-        line[0] = Symbol(line[0])
-    graph[line[0]] = list(exp(line[1]).free_symbols)
-    ind[line[0]] = count
-    count += 1
+        if str(line[0]).startswith('d'):
+            line[0] = str(line[0]).split('d')[1]
+            #print(line[0])
+            line[0] = Symbol(line[0])
+        graph[line[0]] = list(exp(line[1]).free_symbols)
+        ind[line[0]] = count
+        count += 1
 
-f.close()
-print(ind)
-
-print(graph)
-
-
-models = {}
+    f.close()
+    return ind , graph
 
 
-X = [] # set of states
-for value in graph.values():
-    for i in value:
-        if i not in X:
-            X.append(i)
 
-Y = [] # set of outputs
-for key in graph.keys():
-    if key not in Y and str(key).startswith('y'):
-        Y.append(key)
+def list_of_states(graph):
+    
+    X = [] # set of states
+    for value in graph.values():
+        for i in value:
+            if i not in X:
+                X.append(i)
 
-print(X)
-print(Y)
+    Y = [] # set of outputs
+    for key in graph.keys():
+        if key not in Y and str(key).startswith('y'):
+            Y.append(key)
+    return X,Y
+
 
 def dfs(graph, start, path=[]):
     path = path + [start]
@@ -48,50 +47,69 @@ def dfs(graph, start, path=[]):
             path = dfs(graph, node, path)
     return path
 
+def find_models(graph, Y):
+    models = {}
+    for y in Y:
+        models[y] = dfs(graph, y)
+    return models
 
-for y in Y:
-    models[y] = dfs(graph, y)
-
-print(models)
-res = []
-for y in Y:
-    temp = models[y]
-    #print(temp[1:])  
-    l = len(temp[1:])
-    count = 0
-    for i in Y:
-        if i != y:
-            if set(graph[i]).issubset(set(models[y])):
-                models[y].append(i)
+def algorithm(graph, models, Y):
+    for y in Y:
+        for i in Y:
+            if i != y:
+                if set(graph[i]).issubset(set(models[y])):
+                    models[y].append(i)
+    
+    res = []
+    for y in Y:
+        res.append(models[y])
+    return res
             
         
+def output(res, ind, filename):
+    ln = []
+    for i in res:
+        il = []
+        for j in i:
+            il.append(ind[j])
+        ln.append(il)
 
-print(models)
 
-res = []
-for y in Y:
-    res.append(models[y])
 
-print(res)
-ln = []
-for i in res:
-    il = []
-    for j in i:
-        il.append(ind[j])
-    ln.append(il)
+    f = open(f'{filename}.txt', 'r')
+    lines = f.readlines()
+    f.close()
 
-print(ln)
-
-f = open('input.txt', 'r')
-lines = f.readlines()
-f.close()
-
-f = open('submodels.txt', 'w')
-for i in ln:
-    f.write('Submodel ' + str(ln.index(i) + 1) + ':'+'\n')
+    f = open(f'{filename}' + '-res.txt', 'w')
+    for i in ln:
+        f.write('Submodel ' + str(ln.index(i) + 1) + ':'+'\n')
+        
+        for j in i:
+            f.write(lines[j])
     
-    for j in i:
-        f.write(lines[j])
+
+
+    f.close()
+
+
+def main():
+    if len(sys.argv) != 2:
+        print('Usage: python3 project.py <input_file>')
+        return 
+    input = sys.argv[1]
+    ind , graph = read_input(input)
+    X,Y = list_of_states(graph)
+    models = find_models(graph, Y)
+    res = algorithm(graph, models, Y)
+    output(res, ind, input[:-4])
+
+if __name__ == '__main__':
+# measure process time
+    start_time = time.process_time()
+    main()
+    print("Process Time: %s seconds" % (time.process_time() - start_time))
+
+
 
 
 
